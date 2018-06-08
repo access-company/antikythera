de9e158594e780ab6917e4014cf6d6ff0288bd2a
Shunpei Shiina <shunpei.shiina@access-company.com>
2018-06-08 12:44:08 +0900

    [133935] Cache gear config in process dictionary while handling web requests.

--- lib/gear_application/config_getter.ex
+++ lib/gear_application/config_getter.ex
@@ -27,6 +27,32 @@ defmodule Antikythera.GearApplication.ConfigGetter do
 
   @doc false
   def get_all_env(gear_name) do
+    case load_config_from_process_dictionary(gear_name) do
+      {:ok   , config              } -> config
+      {:error, current_gear_configs} ->
+        config = load_config_from_ets(gear_name)
+        store_config_to_process_dictionary(gear_name, current_gear_configs, config)
+        config
+    end
+  end
+
+  defp load_config_from_process_dictionary(gear_name) do
+    case Process.get(:gear_configs) do
+      nil     -> {:error, %{}}
+      configs ->
+        case Map.get(configs, gear_name) do
+          nil    -> {:error, configs}
+          config -> {:ok   , config }
+        end
+    end
+  end
+
+  defp store_config_to_process_dictionary(gear_name, current_gear_configs, config) do
+    new_gear_configs = Map.put(current_gear_configs, gear_name, config)
+    Process.put(:gear_configs, new_gear_configs)
+  end
+
+  defp load_config_from_ets(gear_name) do
     case AntikytheraCore.Ets.ConfigCache.Gear.read(gear_name) do
       nil                                  -> %{}
       %AntikytheraCore.Config.Gear{kv: kv} -> kv
@@ -41,5 +67,10 @@ defmodule Antikythera.GearApplication.ConfigGetter do
   @doc false
   def get_env!(gear_name, key) do
     get_all_env(gear_name) |> Map.fetch!(key)
+  end
+
+  defun cleanup_configs_in_process_dictionary() :: :ok do
+    Process.delete(:gear_configs)
+    :ok
   end
 end
