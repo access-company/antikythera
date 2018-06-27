d120c52902c92c5b89c803d3ac8b9cadd4b44515
Shunpei Shiina <shunpei.shiina@access-company.com>
2018-06-27 13:09:54 +0900

    [133936] Make the table name of ETS a module attribute

--- core/ets/memcache.ex
+++ core/ets/memcache.ex
@@ -6,28 +6,27 @@ defmodule AntikytheraCore.Ets.Memcache do
   alias Croma.Result, as: R
   alias Antikythera.ExecutorPool.Id, as: EPoolId
 
-  defun init() :: :ok do
-    AntikytheraCore.Ets.create_read_optimized_table(table_name())
-  end
+  @table_name :antikythera_memcache
+  defun table_name() :: atom, do: @table_name
 
-  defun table_name() :: atom do
-    :antikythera_memcache
+  defun init() :: :ok do
+    AntikytheraCore.Ets.create_read_optimized_table(@table_name)
   end
 
   defun read(key :: term, epool_id :: v[EPoolId.t]) :: R.t(term, :not_found) do
-    case :ets.lookup(table_name(), {epool_id, key}) do
+    case :ets.lookup(@table_name, {epool_id, key}) do
       []        -> {:error, :not_found}
       [element] -> {:ok   , element}
     end
   end
 
   defun write(key :: term, value :: term, expire_at :: integer, prob_expire_at :: integer, epool_id :: v[EPoolId.t]) :: :ok do
-    :ets.insert(table_name(), {{epool_id, key}, expire_at, prob_expire_at, value})
+    :ets.insert(@table_name, {{epool_id, key}, expire_at, prob_expire_at, value})
     :ok
   end
 
   defun delete(key :: term, epool_id :: v[EPoolId.t]) :: :ok do
-    :ets.delete(table_name(), {epool_id, key})
+    :ets.delete(@table_name, {epool_id, key})
     :ok
   end
 end
