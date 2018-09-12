cc0250f9041778f99559e72395bfe55b8d368281
aMasakiTakahashi <masaki.takahashi11@access-company.com>
2018-09-12 09:13:00 +0900

    [133936] wrapper around foretoken (#30)

---
+++ lib/util/token_bucket.ex
@@ -0,0 +1,23 @@
+# Copyright(c) 2015-2018 ACCESS CO., LTD. All rights reserved.
+
+use Croma
+
+defmodule Antikythera.TokenBucket do
+  @moduledoc """
+  A thin wrapper around [`Foretoken`](https://github.com/skirino/foretoken) to avoid collisions between bucket names by prefixing executor pool IDs.
+
+  For details, refer to [Foretoken's documentation](https://hexdocs.pm/foretoken/api-reference.html).
+  """
+
+  @doc """
+  Usage of this function is the same as `take/4` except that an executor pool ID is required as an argument.
+  """
+  defun take(epool_id               :: v[Antikythera.ExecutorPool.Id.t],
+             bucket                 :: any,
+             milliseconds_per_token :: g[pos_integer],
+             max_tokens             :: g[pos_integer],
+             tokens_to_take         :: g[pos_integer] \\ 1) :: :ok | {:error, pos_integer} do
+    bucket_with_epool_id = {epool_id, bucket}
+    Foretoken.take(bucket_with_epool_id, milliseconds_per_token, max_tokens, tokens_to_take)
+  end
+end
--- mix.exs
+++ mix.exs
@@ -77,6 +77,7 @@ defmodule Antikythera.Mixfile do
       {:rafted_value, "0.10.1", [antikythera_internal: true]},
       {:syn         , "1.6.3" , [antikythera_internal: true]},
       {:fast_xml    , "1.1.32", [antikythera_internal: true]},
+      {:foretoken   , "0.2.0" , [antikythera_internal: true]},
       {:recon       , "2.3.6" , [antikythera_internal: true]},
       {:relx        , "3.23.1", [antikythera_internal: true, only: :prod, runtime: false]}, # only to generate release
 
@@ -86,7 +87,6 @@ defmodule Antikythera.Mixfile do
       {:gettext  , "0.16.0"},
       {:croma    , "0.10.1"},
       {:pbkdf2   , "2.0.0" },
-      {:foretoken, "0.2.0" },
 
       # tools
       {:exsync          , "0.2.3" , [only: :dev ]},
