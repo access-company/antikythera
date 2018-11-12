c657a6b67490aad04284eec18347835cb6b07de5
Tatsuya Toki <tatsuya.toki@access-company.com>
2018-11-12 15:12:37 +0900

    [146100] Remove gulp entry point (#37)
    * Doc: Fix moduledoc
    * Remove `package.json` check processing in `install_packages!/1`

--- lib/mix/prepare_assets.ex
+++ lib/mix/prepare_assets.ex
@@ -13,49 +13,33 @@ defmodule Mix.Tasks.Antikythera.PrepareAssets do
   See `Antikythera.Asset` for details.
 
   If your gear uses some kind of preprocessing tools to generate asset files (JS, CSS, etc.),
-  you have to set up any of the supported asset preparation methods described in the next section.
-
-  This mix task detects available asset preparation methods in your gear repository, then executes them in sequence.
-  If any part of the preparation resulted in failure (non-zero exit code),
-  the whole task will abort and thus auto-deploy will fail.
+  you have to set up the supported asset preparation method described in the next section.
 
   Normally you do not have to invoke this mix task when you locally develop your assets.
   Though you may do so in order to confirm asset preparation is working as you intended.
 
-  This mix task invokes the chosen preprocessing tools with `ANTIKYTHERA_COMPILE_ENV` environment variable
+  This mix task invokes the preprocessing tools with `ANTIKYTHERA_COMPILE_ENV` environment variable
   (see also `Antikythera.Env`).
   You can use this environment variable to distinguish for which environment current script is running.
 
-  ## Supported Asset Preparation Methods
+  ## Supported Asset Preparation Method
 
   Asset preparation process is split into two steps: package installation step and build step.
+  If any part of the preparation process resulted in failure (non-zero exit code),
+  the whole task will abort and thus auto-deploy will fail.
 
-  Any combinations of available methods are acceptable.
+  - Prerequisite: `package.json` file and `antikythera_prepare_assets` script in it
 
-  Note that if none of prerequisites for build steps are present,
+  Note that if the above prerequisite is not present,
   **the whole asset preparation process will be skipped** since package installation is unnecessary.
 
   ### Package Installation
 
-  #### 1. Using [`yarn`](https://yarnpkg.com/en/)
+  Use [`yarn`](https://yarnpkg.com/en/) if `yarn.lock` file exists, otherwise use `npm install`.
 
-  - Prerequisite: `yarn.lock` file
-  - Command: `yarn`
-  - **This method takes precedance over `npm install`**.
+  ### Build using [npm-scripts](https://docs.npmjs.com/misc/scripts)
 
-  #### 2. Using `npm install`
-
-  - Prerequisite: `package.json` file
-  - Command: `npm install`
-
-  ### Build
-
-  #### 1. Using [npm-scripts](https://docs.npmjs.com/misc/scripts)
-
-  - Prerequisite: `antikythera_prepare_assets` script in `package.json` file
   - Command: `npm run antikythera_prepare_assets`
-  - This is the recommended method.
-  - **This method takes precedance over `gulp`**.
   - Within `antikythera_prepare_assets` script, you may execute any asset-related actions such as:
       - Linting
       - Type Checking
@@ -65,26 +49,6 @@ defmodule Mix.Tasks.Antikythera.PrepareAssets do
       - etc...
   - How to organize these actions is up to you. You may use whatever tools available in `npm`,
     such as [`webpack`](https://webpack.js.org/) or [`browserify`](http://browserify.org/).
-      - You can even call `gulp` tasks from the script.
-
-  #### 2. Using [`gulp`](https://gulpjs.com/) (Deprecated)
-
-  - Prerequisite: `gulpfile.js` file
-  - Command: `node_modules/.bin/gulp`
-      - `default` gulp task will be executed.
-  - This is the old and deprecated method, kept for backward compatibility. Use npm-scripts method for new gears.
-  - As is the case in npm-scripts, you may execute any asset-related actions in your `default` gulp task.
-  - You can safely migrate to npm-scripts by putting `antikythera_prepare_assets` script in your `package.json` file like this:
-
-  ```
-  {
-    ...
-    "scripts": {
-      ...
-      "antikythera_prepare_assets": "gulp"
-    }
-  }
-  ```
 
   #### Note on implementation
 
@@ -108,22 +72,13 @@ defmodule Mix.Tasks.Antikythera.PrepareAssets do
   end
 
   defp run_impl(env) do
-    case find_build_step_in_project(env) do
-      nil ->
-        IO.puts("Skipping. Asset preparation is not configured.")
-        IO.puts("Define `antikythera_prepare_assets` npm-script if you want antikythera to prepare assets for your gear.")
-      step ->
-        install_packages!(env)
-        build_assets!(step, env)
-        dump_asset_file_paths()
-    end
-  end
-
-  defunp find_build_step_in_project(env :: v[String.t]) :: nil | :npm_script | :gulp do
-    cond do
-      npm_script_available?(env)  -> :npm_script
-      File.exists?("gulpfile.js") -> :gulp
-      true                        -> nil
+    if npm_script_available?(env) do
+      install_packages!(env)
+      build_assets!(env)
+      dump_asset_file_paths()
+    else
+      IO.puts("Skipping. Asset preparation is not configured.")
+      IO.puts("Define `antikythera_prepare_assets` npm-script if you want antikythera to prepare assets for your gear.")
     end
   end
 
@@ -134,16 +89,11 @@ defmodule Mix.Tasks.Antikythera.PrepareAssets do
   end
 
   defp install_packages!(env) do
-    cond do
-      File.exists?("yarn.lock") ->
-        run_command!("yarn", [], env)
-      File.exists?("package.json") ->
-        remove_node_modules_if_dependencies_changed!()
-        run_command!("npm", ["install"], env)
-      true ->
-        # Both `yarn` and `npm install` will exit with 0 when required files do not exist.
-        # Such cases are considered an error in this context.
-        raise("Missing 'yarn.lock' or 'package.json'.")
+    if File.exists?("yarn.lock") do
+      run_command!("yarn", [], env)
+    else
+      remove_node_modules_if_dependencies_changed!()
+      run_command!("npm", ["install"], env)
     end
   end
 
@@ -161,12 +111,8 @@ defmodule Mix.Tasks.Antikythera.PrepareAssets do
     end
   end
 
-  defp build_assets!(:npm_script, env) do
+  defp build_assets!(env) do
     run_command!("npm", ["run", "antikythera_prepare_assets"], env)
-  end
-  defp build_assets!(:gulp, env) do
-    local_gulp_abs_path = Path.expand(Path.join(["node_modules", ".bin", "gulp"]))
-    run_command!(local_gulp_abs_path, [], env)
   end
 
   def dump_asset_file_paths() do
