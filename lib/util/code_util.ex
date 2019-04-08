5a1cbc41f419870c73d4da45e1770f17688ca2d9
toyoaki-k <toyoaki.kato@access-company.com>
2019-04-08 18:45:00 +0900

    [150737] Align code

--- lib/util/code_util.ex
+++ lib/util/code_util.ex
@@ -15,8 +15,8 @@ defmodule Antikythera.CodeUtil do
   defun doc_by_mfa!(module :: v[module], function :: v[atom], arity :: v[non_neg_integer]) :: String.t do
     {:docs_v1, _anno, _beam_lang, _format, _moduledoc, _meta, docs} = Code.fetch_docs(module)
     case List.keyfind(docs, {:function, function, arity}, 0) do
-     {_tuple3, _anno, _signature, %{"en" => doc}, _meta} -> doc
-     _otherwise                                          -> raise(ArgumentError, "Not found: #{inspect(module)}.#{function}/#{arity}")
+      {_tuple3, _anno, _signature, %{"en" => doc}, _meta} -> doc
+      _otherwise                                          -> raise(ArgumentError, "Not found: #{inspect(module)}.#{function}/#{arity}")
     end
   end
 end
