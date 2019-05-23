68865f7a2fbac8512474cf5df8a62f3f1f900537
aNobuhisaMasaki <nobuhisa.masaki@access-company.com>
2019-05-23 16:44:26 +0900

    [177185] Whitebox test case: outside tmpdir

--- test/lib/util/zip_test.exs
+++ test/lib/util/zip_test.exs
@@ -2,6 +2,7 @@
 
 defmodule Antikythera.ZipTest do
   use Croma.TestCase
+  alias AntikytheraCore.TmpdirTracker
   alias Antikythera.{Tmpdir, Zip}
 
   setup do
@@ -41,6 +42,14 @@ defmodule Antikythera.ZipTest do
           assert Zip.zip(@context, zip_path, tmpdir <> path_to_archive, [encryption: true, password: "password"]) == {:ok, zip_path}
         end)
       end)
+    end
+
+    test "returns error when tmpdir is not found" do
+      :meck.expect(TmpdirTracker, :get, fn _ -> {:error, {:not_found, %{}}} end)
+      zip_path = "/tmpdir/archive.zip"
+      src_path = "/tmpdir/src.txt"
+      :meck.expect(File, :exists?, fn _ -> flunk() end)
+      assert Zip.zip(@context, zip_path, src_path) == {:error, {:not_found, %{}}}
     end
   end
 end
