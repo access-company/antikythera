6e4ad8e4ddbaa95f8ac608667e0fc67081955bf6
aNobuhisaMasaki <nobuhisa.masaki@access-company.com>
2019-05-23 16:44:25 +0900

    [177185] Test get interface in tmpdir tracker

---
+++ test/core/sup_tree_core/tmpdir_tracker_test.exs
@@ -0,0 +1,22 @@
+# Copyright(c) 2015-2019 ACCESS CO., LTD. All rights reserved.
+
+defmodule AntikytheraCore.TmpdirTrackerTest do
+  use ExUnit.Case
+  alias Antikythera.Tmpdir
+  alias AntikytheraCore.TmpdirTracker
+
+  @context Antikythera.Test.ConnHelper.make_conn().context
+
+  test "TmpdirTracker.get/1 returns tmpdir only within Tmpdir.make/2" do
+    assert %{
+      tmpdir: tmpdir,
+      actual: {:ok, tmpdir},
+    } = Tmpdir.make(@context, fn tmpdir ->
+      %{
+        tmpdir: tmpdir,
+        actual: TmpdirTracker.get(@context.executor_pool_id),
+      }
+    end)
+    assert {:error, {:not_found, _}} = TmpdirTracker.get(@context.executor_pool_id)
+  end
+end
