1bb0d20e906dc822ebafafdd7b862a0e2f03bef3
aNobuhisaMasaki <nobuhisa.masaki@access-company.com>
2019-05-28 18:08:01 +0900

    [177185] More simple testing for tmpdir tracker

--- test/core/sup_tree_core/tmpdir_tracker_test.exs
+++ test/core/sup_tree_core/tmpdir_tracker_test.exs
@@ -8,15 +8,9 @@ defmodule AntikytheraCore.TmpdirTrackerTest do
   @context Antikythera.Test.ConnHelper.make_conn().context
 
   test "TmpdirTracker.get/1 returns tmpdir only within Tmpdir.make/2" do
-    assert %{
-      tmpdir: tmpdir,
-      actual: {:ok, tmpdir},
-    } = Tmpdir.make(@context, fn tmpdir ->
-      %{
-        tmpdir: tmpdir,
-        actual: TmpdirTracker.get(@context.executor_pool_id),
-      }
+    Tmpdir.make(@context, fn tmpdir ->
+      assert {:ok, tmpdir} == TmpdirTracker.get(@context.executor_pool_id)
     end)
-    assert {:error, :not_found} = TmpdirTracker.get(@context.executor_pool_id)
+    assert {:error, :not_found} == TmpdirTracker.get(@context.executor_pool_id)
   end
 end
