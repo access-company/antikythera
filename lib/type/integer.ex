5e708bff7a4ab99e50a09b24af51cfb4eab157a3
aDaichiOta <daichi.ota@access-company.com>
2019-07-24 09:34:55 +0900

    [177930] Remove blank line

--- lib/type/integer.ex
+++ lib/type/integer.ex
@@ -10,7 +10,6 @@ end
 
 defmodule Antikythera.MilliSecondsInGregorian do
   @time_epoch_offset_milliseconds (:calendar.datetime_to_gregorian_seconds({{1970, 1, 1}, {0, 0, 0}}) * 1000)
-
   def time_epoch_offset_milliseconds(), do: @time_epoch_offset_milliseconds
 
   # This restriction is temporarily expanded for backward compatibility.
