395043a53d98e3a9041a69375b3080c83c738307
aDaichiOta <daichi.ota@access-company.com>
2019-08-13 18:21:15 +0900

    [185147] Check log write (#59)

--- core/gear_log/file_handle.ex
+++ core/gear_log/file_handle.ex
@@ -50,7 +50,7 @@ defmodule AntikytheraCore.GearLog.FileHandle do
   defunp do_write(io_device :: :file.io_device, {time, level, context_id, msg} :: Message.t) :: :ok do
     prefix = log_prefix(time, level, context_id)
     formatted_lines_str = String.split(msg, "\n", trim: true) |> Enum.map_join(&(prefix <> &1 <> "\n"))
-    IO.binwrite(io_device, formatted_lines_str)
+    :ok = IO.binwrite(io_device, formatted_lines_str)
     write_debug_log(level, formatted_lines_str)
   end
 
