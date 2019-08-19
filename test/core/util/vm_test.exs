02d5887ceacb9442857a9523c8a9817fd383990f
aDaichiOta <daichi.ota@access-company.com>
2019-08-19 15:25:32 +0900

    [184651] count messages in all mailboxes (#63)

---
+++ test/core/util/vm_test.exs
@@ -0,0 +1,11 @@
+# Copyright(c) 2015-2019 ACCESS CO., LTD. All rights reserved.
+
+defmodule AntikytheraCore.VmTest do
+  use Croma.TestCase
+  alias AntikytheraCore.Vm
+
+  test "count_messages_in_all_mailboxes/0" do
+    count = Vm.count_messages_in_all_mailboxes()
+    assert count >= 0
+  end
+end
