e01eeb99790a1879476a5d53c6980410b98abab9
aDaichiOta <daichi.ota@access-company.com>
2019-08-20 13:17:56 +0900

    Fix lib/util/{string_format, time}.ex

--- lib/util/string_format.ex
+++ lib/util/string_format.ex
@@ -4,13 +4,13 @@ use Croma
 
 defmodule Antikythera.StringFormat do
   defun pad2(int :: non_neg_integer) :: String.t do
-    i when i <  10 -> "0#{i}"
+    i when i <  10 -> <<"0", Integer.to_string(i) :: binary-size(1)>>
     i when i < 100 -> Integer.to_string(i)
   end
 
   defun pad3(int :: non_neg_integer) :: String.t do
-    i when i <   10 -> "00#{i}"
-    i when i <  100 -> "0#{i}"
+    i when i <   10 -> <<"00", Integer.to_string(i) :: binary-size(1)>>
+    i when i <  100 -> <<"0" , Integer.to_string(i) :: binary-size(2)>>
     i when i < 1000 -> Integer.to_string(i)
   end
 end
