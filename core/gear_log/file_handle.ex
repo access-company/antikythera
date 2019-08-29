a70974e362fd2831f49cec496f94d8df1f5c256e
aDaichiOta <daichi.ota@access-company.com>
2019-08-29 15:09:51 +0900

    rename variable

--- core/gear_log/file_handle.ex
+++ core/gear_log/file_handle.ex
@@ -23,47 +23,47 @@ defmodule AntikytheraCore.GearLog.FileHandle do
   @opaque t :: {Path.t, Time.t, File.io_device, boolean}
 
   defun open(file_path :: Path.t, opts \\ []) :: t do
-    write_to_terminal = Keyword.get(opts, :write_to_terminal, determine_write_to_terminal())
+    write_to_terminal? = Keyword.get(opts, :write_to_terminal, determine_write_to_terminal())
     :ok = File.mkdir_p(Path.dirname(file_path))
     if File.exists?(file_path) do
       rename(file_path)
     end
-    {file_path, Time.now(), open_file(file_path), write_to_terminal}
+    {file_path, Time.now(), open_file(file_path), write_to_terminal?}
   end
 
-  defun write({file_path, last_checked_at, io_device, write_to_terminal} = handle   :: t,
+  defun write({file_path, last_checked_at, io_device, write_to_terminal?} = handle   :: t,
               {now, _, _, _}                                             = gear_log :: Message.t) :: {:kept_open | :rotated, t} do
     if SizeCheck.check_now?(now, last_checked_at) do
       if SizeCheck.exceeds_limit?(file_path) do
         {_, _, new_io_device, _} = new_handle = rotate(handle)
-        do_write(new_io_device, gear_log, write_to_terminal)
+        do_write(new_io_device, gear_log, write_to_terminal?)
         {:rotated, new_handle}
       else
-        do_write(io_device, gear_log, write_to_terminal)
-        {:kept_open, {file_path, now, io_device, write_to_terminal}}
+        do_write(io_device, gear_log, write_to_terminal?)
+        {:kept_open, {file_path, now, io_device, write_to_terminal?}}
       end
     else
-      do_write(io_device, gear_log, write_to_terminal)
+      do_write(io_device, gear_log, write_to_terminal?)
       {:kept_open, handle}
     end
   end
 
-  defunp do_write(io_device :: :file.io_device, {time, level, context_id, msg} :: Message.t, write_to_terminal :: boolean) :: :ok do
+  defunp do_write(io_device :: :file.io_device, {time, level, context_id, msg} :: Message.t, write_to_terminal? :: boolean) :: :ok do
     prefix = log_prefix(time, level, context_id)
     formatted_lines_str = String.split(msg, "\n", trim: true)
       |> Enum.reduce("", fn(s, acc) -> acc <> prefix <> s <> "\n" end)
     :ok = IO.binwrite(io_device, formatted_lines_str)
-    if write_to_terminal, do: write_debug_log(level, formatted_lines_str), else: :ok
+    if write_to_terminal?, do: write_debug_log(level, formatted_lines_str), else: :ok
   end
 
   defunp log_prefix(time :: v[Time.t], level :: v[Level.t], context_id :: v[ContextId.t]) :: String.t do
     Time.to_iso_timestamp(time) <> " [" <> Atom.to_string(level) <> "] context=" <> context_id <> " "
   end
 
-  defun rotate({file_path, _, io_device, write_to_terminal} :: t) :: t do
+  defun rotate({file_path, _, io_device, write_to_terminal?} :: t) :: t do
     :ok = File.close(io_device)
     rename(file_path)
-    {file_path, Time.now(), open_file(file_path), write_to_terminal}
+    {file_path, Time.now(), open_file(file_path), write_to_terminal?}
   end
 
   defunp open_file(file_path :: Path.t) :: File.io_device do
