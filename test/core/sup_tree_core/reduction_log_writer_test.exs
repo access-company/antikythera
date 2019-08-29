7a017544fe87adcab9796695fe48434aeb7e796e
aDaichiOta <daichi.ota@access-company.com>
2019-08-29 14:05:44 +0900

    Update test/core/sup_tree_core/reduction_log_writer_test.exs
    Co-Authored-By: Tatsuya Toki <tatsuya.toki@access-company.com>

--- test/core/sup_tree_core/reduction_log_writer_test.exs
+++ test/core/sup_tree_core/reduction_log_writer_test.exs
@@ -4,7 +4,7 @@ defmodule AntikytheraCore.ReductionLogWriterTest do
   @dir Path.join([__DIR__, "..", "..", "..", "_build", "test", "log", "antikythera"]) |> Path.expand()
 
   test "should exists" do
-    assert Process.whereis(AntikytheraCore.ReductionLogWriter) != nil
+    assert Process.whereis(ReductionLogWriter) != nil
     send(AntikytheraCore.ReductionLogWriter, :timeout)
     assert File.exists?(Path.join(@dir, "reduction.log.gz"))
   end
