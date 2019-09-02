c3330c8f9cf0a45efbac854ad453d6bcf7178c04
aDaichiOta <daichi.ota@access-company.com>
2019-09-02 16:24:14 +0900

    [186119] Add reduction writer (#78)

--- core/gear_log/file_handle.ex
+++ core/gear_log/file_handle.ex
@@ -20,56 +20,57 @@ defmodule AntikytheraCore.GearLog.FileHandle do
     end
   end
 
-  @opaque t :: {Path.t, Time.t, File.io_device}
+  @opaque t :: {Path.t, Time.t, File.io_device, boolean}
 
-  defun open(file_path :: Path.t) :: t do
+  defun open(file_path :: Path.t, opts :: Keyword.t \\ []) :: t do
+    write_to_terminal? = Keyword.get(opts, :write_to_terminal, determine_write_to_terminal())
     :ok = File.mkdir_p(Path.dirname(file_path))
     if File.exists?(file_path) do
       rename(file_path)
     end
-    {file_path, Time.now(), open_file(file_path)}
+    {file_path, Time.now(), open_file(file_path), write_to_terminal?}
   end
 
-  defun write({file_path, last_checked_at, io_device} = handle   :: t,
-              {now, _, _, _}                          = gear_log :: Message.t) :: {:kept_open | :rotated, t} do
+  defun write({file_path, last_checked_at, io_device, write_to_terminal?} = handle   :: t,
+              {now, _, _, _}                                              = gear_log :: Message.t) :: {:kept_open | :rotated, t} do
     if SizeCheck.check_now?(now, last_checked_at) do
       if SizeCheck.exceeds_limit?(file_path) do
-        {_, _, new_io_device} = new_handle = rotate(handle)
-        do_write(new_io_device, gear_log)
+        {_, _, new_io_device, _} = new_handle = rotate(handle)
+        do_write(new_io_device, gear_log, write_to_terminal?)
         {:rotated, new_handle}
       else
-        do_write(io_device, gear_log)
-        {:kept_open, {file_path, now, io_device}}
+        do_write(io_device, gear_log, write_to_terminal?)
+        {:kept_open, {file_path, now, io_device, write_to_terminal?}}
       end
     else
-      do_write(io_device, gear_log)
+      do_write(io_device, gear_log, write_to_terminal?)
       {:kept_open, handle}
     end
   end
 
-  defunp do_write(io_device :: :file.io_device, {time, level, context_id, msg} :: Message.t) :: :ok do
+  defunp do_write(io_device :: :file.io_device, {time, level, context_id, msg} :: Message.t, write_to_terminal? :: boolean) :: :ok do
     prefix = log_prefix(time, level, context_id)
     formatted_lines_str = String.split(msg, "\n", trim: true)
       |> Enum.reduce("", fn(s, acc) -> acc <> prefix <> s <> "\n" end)
     :ok = IO.binwrite(io_device, formatted_lines_str)
-    write_debug_log(level, formatted_lines_str)
+    if write_to_terminal?, do: write_debug_log(level, formatted_lines_str), else: :ok
   end
 
   defunp log_prefix(time :: v[Time.t], level :: v[Level.t], context_id :: v[ContextId.t]) :: String.t do
     Time.to_iso_timestamp(time) <> " [" <> Atom.to_string(level) <> "] context=" <> context_id <> " "
   end
 
-  defun rotate({file_path, _, io_device} :: t) :: t do
+  defun rotate({file_path, _, io_device, write_to_terminal?} :: t) :: t do
     :ok = File.close(io_device)
     rename(file_path)
-    {file_path, Time.now(), open_file(file_path)}
+    {file_path, Time.now(), open_file(file_path), write_to_terminal?}
   end
 
   defunp open_file(file_path :: Path.t) :: File.io_device do
     File.open!(file_path, [:write, :compressed])
   end
 
-  defun close({_, _, io_device} :: t) :: :ok do
+  defun close({_, _, io_device, _} :: t) :: :ok do
     :ok = File.close(io_device)
   end
 
@@ -84,20 +85,16 @@ defmodule AntikytheraCore.GearLog.FileHandle do
     String.replace(base_file_path, ~R/gz\z/, now_str_with_ext)
   end
 
-  if Antikythera.Env.compiling_for_release?() do
-    defunp write_debug_log(_ :: Level.t, _ :: String.t) :: :ok, do: :ok
-  else
-    defunp write_debug_log(level :: v[Level.t], formatted :: v[String.t]) :: :ok do
-      if Mix.env() == :dev do
-        case level do
-          :error -> IO.ANSI.red() <> formatted <> IO.ANSI.reset()
-          :debug -> IO.ANSI.cyan() <> formatted <> IO.ANSI.reset()
-          _      -> formatted
-        end
-        |> IO.binwrite()
-      else
-        :ok # don't put logs during tests
-      end
+  defunp write_debug_log(level :: v[Level.t], formatted :: v[String.t]) :: :ok do
+    case level do
+      :error -> IO.ANSI.red()  <> formatted <> IO.ANSI.reset()
+      :debug -> IO.ANSI.cyan() <> formatted <> IO.ANSI.reset()
+      _      -> formatted
     end
+    |> IO.binwrite()
+  end
+
+  defp determine_write_to_terminal() do
+    !Antikythera.Env.compiling_for_release?() && Mix.env() == :dev
   end
 end
---
+++ core/sup_tree_core/reduction_log_writer.ex
@@ -0,0 +1,106 @@
+# Copyright(c) 2015-2019 ACCESS CO., LTD. All rights reserved.
+
+use Croma
+
+defmodule AntikytheraCore.ReductionLogWriter do
+  @moduledoc """
+  A `GenServer` that logs processes with increasing reduction.
+  """
+
+  use GenServer
+  alias AntikytheraCore.GearLog.FileHandle
+  alias Antikythera.{Time, ContextId}
+
+  @interval        1000
+  @dump_size       20
+  @rotate_interval 7_200_000
+
+  defmodule State do
+    use Croma.Struct, recursive_new?: true, fields: [
+      file_handle: Croma.Tuple, # FileHandle.t
+      empty?:      Croma.Boolean,
+      timer:       Croma.Reference,
+      reduction:   Croma.Map,
+    ]
+  end
+
+  def start_link([]) do
+    GenServer.start_link(__MODULE__, [], [name: __MODULE__])
+  end
+
+  @impl true
+  def init([]) do
+    handle = FileHandle.open(AntikytheraCore.Path.core_log_file_path("reduction"), write_to_terminal: false)
+    timer = arrange_next_rotation(nil)
+    reduction = get_reduction_map()
+    {:ok, %State{file_handle: handle, empty?: true, timer: timer, reduction: reduction}, @interval}
+  end
+
+  @impl true
+  def handle_info(:timeout, %State{file_handle: handle, timer: timer, reduction: prev_reduction} = state) do
+    {message, reduction} = build_log(prev_reduction)
+    msg = {Time.now(), :info, ContextId.system_context(), message}
+    case FileHandle.write(handle, msg) do
+      {:kept_open, new_handle} -> {:noreply, %State{state | file_handle: new_handle, empty?: false, reduction: reduction}, @interval}
+      {:rotated  , new_handle} ->
+        # Log file is just rotated as its size has exceeded the upper limit.
+        # Note that the current message is written to the newly-opened log file and thus it's not empty.
+        new_timer = arrange_next_rotation(timer)
+        {:noreply, %State{state | file_handle: new_handle, empty?: false, timer: new_timer, reduction: reduction}, @interval}
+    end
+  end
+  def handle_info(:rotate, state) do
+    {:noreply, rotate(state), @interval}
+  end
+
+  defp rotate(%State{file_handle: handle, empty?: empty?, timer: timer} = state0) do
+    new_timer = arrange_next_rotation(timer)
+    state1 = %State{state0 | timer: new_timer}
+    if empty? do
+      state1
+    else
+      %State{state1 | file_handle: FileHandle.rotate(handle), empty?: true}
+    end
+  end
+
+  @impl true
+  def terminate(_reason, %State{file_handle: handle}) do
+    FileHandle.close(handle)
+  end
+
+  defp arrange_next_rotation(timer) do
+    if timer != nil do
+      Process.cancel_timer(timer)
+    end
+    Process.send_after(self(), :rotate, @rotate_interval)
+  end
+
+  defp build_log(prev_reduction) do
+    log_time = Antikythera.Time.to_iso_timestamp(Antikythera.Time.now())
+    current_reduction = get_reduction_map()
+    msg =
+      make_diff(current_reduction, prev_reduction)
+      |> Enum.take(@dump_size)
+      |> Enum.reduce(log_time, fn(diff, acc) ->
+        acc <> "\n" <> inspect(diff)
+      end)
+    {msg, current_reduction}
+  end
+
+  defp get_reduction_map() do
+    # If the second argument (number of processes to be acquired) is too small,
+    # the difference in reduction cannot be calculated correctly. But if it is
+    # too large, the load is high. So sufficiently large value is specified.
+    # The number of processes in a normal production environment is less than 5,000.
+    :recon.proc_count(:reductions, 20_000) |> Map.new(fn {pid, count, other} -> {pid, {count, other}} end)
+  end
+
+  defp make_diff(current, prev) do
+    current
+    |> Enum.map(fn {pid, {count, other}} ->
+      prev_cnt = Map.get(prev, pid, {0, nil}) |> elem(0)
+      {pid, {count - prev_cnt, other}}
+    end)
+    |> Enum.sort_by(fn {_pid, {c, _}} -> c end, &>=/2)
+  end
+end
