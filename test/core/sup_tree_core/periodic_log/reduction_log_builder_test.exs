f5fabba0754dab100ef779c990bf64013e4acd8d
irisTa56 <takayuki.kobayashi@access-company.com>
2019-09-27 11:44:22 +0900

    [188089] confirm if state has been updated in `ReductionBuilderTest`

--- test/core/sup_tree_core/periodic_log/reduction_log_builder_test.exs
+++ test/core/sup_tree_core/periodic_log/reduction_log_builder_test.exs
@@ -3,9 +3,23 @@ defmodule AntikytheraCore.PeriodicLog.ReductionBuilderTest do
 
   @dir Path.join([__DIR__, "..", "..", "..", "..", "_build", "test", "log", "antikythera"]) |> Path.expand()
 
-  test "should exists" do
-    assert Process.whereis(ReductionBuilder) != nil
-    send(ReductionBuilder, :timeout)
+  test "should exist" do
+    pid = Process.whereis(ReductionBuilder)
+    assert pid != nil
     assert File.exists?(Path.join(@dir, "reduction.log.gz"))
+
+    state1 = :sys.get_state(pid)
+    build_state1 = state1.build_state
+    # NOTE: The below `accert` statement won't pass. The process probably left its initial state
+    # because it has been running from the beginning of the test.
+    # assert state1.log_state.empty?
+
+    send(ReductionBuilder, :timeout)
+
+    state2 = :sys.get_state(pid)
+    build_state2 = state2.build_state
+    refute state2.log_state.empty?
+
+    refute Map.equal?(build_state1, build_state2) # ensure that the build state has been updated
   end
 end
