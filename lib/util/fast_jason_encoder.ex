71d7dca5ed1cfaa9f7855e7b4794ec21c4ae05fa
aDaichiOta <daichi.ota@access-company.com>
2019-10-08 10:38:05 +0900

    refactoring

--- lib/util/fast_jason_encoder.ex
+++ lib/util/fast_jason_encoder.ex
@@ -1,5 +1,7 @@
 # Copyright(c) 2015-2019 ACCESS CO., LTD. All rights reserved.
 
+use Croma
+
 defmodule Antikythera.FastJasonEncoder do
   @moduledoc """
   This module converts structures to JSON at low load by avoiding protocol calls. Internally Jason is used.
@@ -20,49 +22,29 @@ defmodule Antikythera.FastJasonEncoder do
     Jason.encode(%Wrapper{item: value})
   end
 
-  def encode(value, opts) when is_list(value) do
-    case value do
-      []   -> "[]"
-      list -> [?[ | encode_list(list, opts, false)]
-    end
-  end
-  def encode(value, opts) when is_map(value) do
-    case Map.to_list(value) do
-      []   -> "{}"
-      list -> [?{ | encode_map(list, opts, false)]
-    end
-  end
-  def encode({Time, _ymd, _hms, _ms} = time, opts) do
-    if Time.valid?(time) do
-      [?", Time.to_iso_timestamp(time), ?"]
-    else
-      Jason.Encode.value(time, opts)
-    end
-  end
-  def encode(value, opts) do
-    Jason.Encode.value(value, opts)
+  defun encode(value :: any, opts :: Jason.Encode.opts) :: iodata do
+    ([]       , _   )                       -> "[]"
+    (list     , opts) when is_list(list)    -> [?[ | encode_list(list, opts, false)]
+    (empty_map, _   ) when empty_map == %{} -> "{}"
+    (map      , opts) when is_map(map)      -> [?{ | encode_map(Map.to_list(map), opts, false)]
+    (value    , opts)                       ->
+      if Time.valid?(value) do
+        [?", Time.to_iso_timestamp(value), ?"]
+      else
+        Jason.Encode.value(value, opts)
+      end
   end
 
-  def encode_map([], _opts, _comma) do
-    '}'
-  end
-  def encode_map([{:__struct__, _v} | tail], opts, comma) do
-    encode_map(tail, opts, comma)
-  end
-  def encode_map(list, opts, true) do
-    [?,, encode_map(list, opts, false)]
-  end
-  def encode_map([{k, v} | tail], opts, _comma) do
-    [encode(k, opts), ?:, encode(v, opts) | encode_map(tail, opts, true)]
+  defunp encode_map(value :: [tuple], opts :: Jason.Encode.opts, comma :: boolean) :: iodata do
+    ([]                        , _   , _    ) -> '}'
+    ([{:__struct__, _v} | tail], opts, comma) -> encode_map(tail, opts, comma)
+    (list                      , opts, true ) -> [?,, encode_map(list, opts, false)]
+    ([{k,            v} | tail], opts, false) -> [encode(k, opts), ?:, encode(v, opts) | encode_map(tail, opts, true)]
   end
 
-  def encode_list([], _opts, _comma) do
-    ']'
-  end
-  def encode_list(list, opts, true) do
-    [?,, encode_list(list, opts, false)]
-  end
-  def encode_list([head | tail], opts, _comma) do
-    [encode(head, opts) | encode_list(tail, opts, true)]
+  defunp encode_list(value :: list, opts :: Jason.Encode.opts, comma :: boolean) :: iodata do
+    ([]           , _   , _    ) -> ']'
+    (list         , opts, true ) -> [?,, encode_list(list, opts, false)]
+    ([head | tail], opts, false) -> [encode(head, opts) | encode_list(tail, opts, true)]
   end
 end
