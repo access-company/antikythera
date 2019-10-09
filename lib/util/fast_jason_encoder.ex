2dc2022301c678e552e8479a19cf77d3fd32bedf
aDaichiOta <daichi.ota@access-company.com>
2019-10-09 15:14:29 +0900

    [134410] Speed up Conn.json (#95)

---
+++ lib/util/fast_jason_encoder.ex
@@ -0,0 +1,52 @@
+# Copyright(c) 2015-2019 ACCESS CO., LTD. All rights reserved.
+
+use Croma
+
+defmodule Antikythera.FastJasonEncoder do
+  @moduledoc """
+  This module converts structures to JSON at low load by avoiding protocol calls. Internally Jason is used.
+  """
+
+  alias Croma.Result, as: R
+  alias Antikythera.Time
+
+  defmodule Wrapper do
+    defstruct item: nil
+  end
+
+  defimpl Jason.Encoder, for: Wrapper do
+    def encode(%Wrapper{item: item}, opts) do
+      Antikythera.FastJasonEncoder.encode(item, opts)
+    end
+  end
+
+  defun encode(value :: any) :: R.t(String.t, Jason.EncodeError.t | Exception.t) do
+    Jason.encode(%Wrapper{item: value})
+  end
+
+  defun encode(value :: any, opts :: Jason.Encode.opts) :: iodata do
+    ([]       , _   )                       -> "[]"
+    (list     , opts) when is_list(list)    -> [?[ | encode_list(list, opts, false)]
+    (empty_map, _   ) when empty_map == %{} -> "{}"
+    (map      , opts) when is_map(map)      -> [?{ | encode_map(Map.to_list(map), opts, false)]
+    (value    , opts)                       ->
+      if Time.valid?(value) do
+        [?", Time.to_iso_timestamp(value), ?"]
+      else
+        Jason.Encode.value(value, opts)
+      end
+  end
+
+  defunp encode_map(value :: [tuple], opts :: Jason.Encode.opts, comma :: boolean) :: iodata do
+    ([]                        , _   , _    ) -> '}'
+    ([{:__struct__, _v} | tail], opts, comma) -> encode_map(tail, opts, comma)
+    (list                      , opts, true ) -> [?,, encode_map(list, opts, false)]
+    ([{k,            v} | tail], opts, false) -> [encode(k, opts), ?:, encode(v, opts) | encode_map(tail, opts, true)]
+  end
+
+  defunp encode_list(value :: list, opts :: Jason.Encode.opts, comma :: boolean) :: iodata do
+    ([]           , _   , _    ) -> ']'
+    (list         , opts, true ) -> [?,, encode_list(list, opts, false)]
+    ([head | tail], opts, false) -> [encode(head, opts) | encode_list(tail, opts, true)]
+  end
+end
---
+++ test/lib/util/fast_json_encoder_test.exs
@@ -0,0 +1,67 @@
+# Copyright(c) 2015-2019 ACCESS CO., LTD. All rights reserved.
+
+defmodule Antikythera.FastJasonEncoderTest do
+  use Croma.TestCase
+  use ExUnitProperties
+
+  defp assert_compatible_with_poison(value) do
+    assert_compatible_with_poison(value, value)
+  end
+
+  defp assert_compatible_with_poison(source, expected) do
+    {:ok, json} = FastJasonEncoder.encode(source)
+    assert json == Poison.encode!(source)
+    assert Poison.decode!(json) == expected
+  end
+
+  test "primitives" do
+    values = [0, 1, 1.5, true, false, nil]
+    Enum.each(values, &assert_compatible_with_poison/1)
+    assert_compatible_with_poison(:ok, "ok") # Atom is encoded as a string
+  end
+
+  test "complex types" do
+    values = [[], [1.5], [1, "foo"], %{}, %{"key" => "value"}]
+    Enum.each(values, &assert_compatible_with_poison/1)
+
+    # Atom is encoded as a string
+    assert_compatible_with_poison(%{key: "value"}, %{"key" => "value"})
+    assert_compatible_with_poison(%{key: :ok    }, %{"key" => "ok"   })
+
+    # Skip `FastJasonEncoder.encode/1` vs `Poison.encode/1` because the order of the field may change.
+    obj = %{"k1" => "v1", "k2" => "v2"}
+    {:ok, json} = FastJasonEncoder.encode(obj)
+    assert obj == Poison.decode!(json)
+  end
+
+  test "Antikythera.Time" do
+    time = {Antikythera.Time, {2017, 1, 1}, {0, 0, 0}, 0}
+    values = [time, [time], %{time: time}]
+    Enum.each(values, fn(value) ->
+      {:ok, json} = FastJasonEncoder.encode(value)
+      assert json == Poison.encode!(value)
+    end)
+  end
+
+  test "string compatibility" do
+    values = ["\\500", "\"", "foo\nbar", "</script>", "日本語"]
+    Enum.each(values, &assert_compatible_with_poison/1)
+  end
+
+  defmodule MyContainer do
+    defstruct content: nil
+  end
+
+  test "module" do
+    values = [
+      %MyContainer{content: nil},
+      %MyContainer{content: "item"},
+      %MyContainer{content: %MyContainer{content: %MyContainer{content: "item"}}}
+    ]
+    Enum.each(values, fn(value) ->
+      {:ok, json} = FastJasonEncoder.encode(value)
+      assert json == Poison.encode!(value)
+      assert json == Poison.encode!(Map.from_struct(value))
+    end)
+  end
+end
