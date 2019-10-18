4cf9d1064e40e7b49817e722caac2e1d91f7d62d
irisTa56 <takayuki.kobayashi@access-company.com>
2019-10-18 11:44:52 +0900

    [190040] add a module to generate CloudFront signed URL

---
+++ lib/util/cloudfront_signed_url.ex
@@ -0,0 +1,87 @@
+# Copyright(c) 2015-2019 ACCESS CO., LTD. All rights reserved.
+
+use Croma
+
+defmodule Antikythera.CloudfrontSignedUrl do
+  @moduledoc """
+  This module provides `get_signed_url/4` to generate [a signed URL for CloudFront](https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-signed-urls.html)
+  Note that currently we support only a signed URL using a *canned policy*.
+  """
+
+  @doc """
+  Generates a signed URL to access a file via CloudFront.
+
+  ## Parameters
+
+  - `resource_url` (string): CloudFront URL used for accessing the file, including query string parameters, if any.
+  - `lifetime_in_seconds` (positive integer): Expiration time is determined as the sum of the current time (in seconds) and this value.
+  - `key_pair_id` (string): ID for an active CloudFront key pair used for generating the signature.
+  - `private_key` (string): RSA private key for the key pair specified by `key_pair_id`.
+
+  ## Return value
+
+  A generated signed URL (string).
+  """
+  defun get_signed_url(resource_url        :: v[String.t],
+                       lifetime_in_seconds :: v[pos_integer],
+                       key_pair_id         :: v[String.t],
+                       private_key         :: v[String.t]) :: String.t do
+    expires_in_seconds = System.system_time(:second) + lifetime_in_seconds
+    resource_url
+    |> URI.parse()
+    |> Map.update!(:query, fn query ->
+      case query do
+        nil   -> []
+        query -> URI.query_decoder(query)
+      end
+      |> Stream.concat(make_query_params(resource_url, expires_in_seconds, key_pair_id, private_key))
+      |> URI.encode_query()
+    end)
+    |> URI.to_string()
+  end
+
+  defunp make_query_params(resource_url       :: v[String.t],
+                           expires_in_seconds :: v[pos_integer],
+                           key_pair_id        :: v[String.t],
+                           private_key        :: v[String.t]) :: [{String.t, String.t}] do
+    statement = create_policy_statement(resource_url, expires_in_seconds)
+    signature = create_signature(statement, private_key)
+    [
+      {"Expires",     expires_in_seconds},
+      {"Signature",   signature         },
+      {"Key-Pair-Id", key_pair_id       },
+    ]
+  end
+
+  defunp create_policy_statement(resource_url :: v[String.t], expires_in_seconds :: v[pos_integer]) :: String.t do
+    %{
+      "Statement" => [
+        %{
+          "Resource" => resource_url,
+          "Condition" => %{
+            "DateLessThan" => %{
+              "AWS:EpochTime" => expires_in_seconds,
+            },
+          },
+        },
+      ],
+    }
+    |> Antikythera.FastJasonEncoder.encode()
+    |> Croma.Result.get!()
+  end
+
+  defunp create_signature(policy_statement :: v[String.t], private_key :: v[String.t]) :: String.t do
+    :public_key.sign(policy_statement, :sha, decode_rsa_key(private_key))
+    |> Base.encode64()
+    # Replace characters that are invalid in a URL query string with characters that are valid.
+    # https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-creating-signed-url-canned-policy.html
+    |> String.replace("+", "-")
+    |> String.replace("=", "_")
+    |> String.replace("/", "~")
+  end
+
+  defp decode_rsa_key(rsa_key) do
+    [pem_entry] = :public_key.pem_decode(rsa_key)
+    :public_key.pem_entry_decode(pem_entry)
+  end
+end
---
+++ test/lib/util/cloudfront_signed_url_test.exs
@@ -0,0 +1,78 @@
+# Copyright(c) 2015-2019 ACCESS CO., LTD. All rights reserved.
+
+defmodule Antikythera.CloudfrontSignedUrlTest do
+  use Croma.TestCase
+  alias Antikythera.CloudfrontSignedUrl
+
+  @resource_url       "http://123456abcdefg.cloudfront.net/index.html"
+  @expires_in_seconds 2_147_483_646
+  @key_pair_id        "ABCDEFGHIJKLMNOPQRST"
+  @private_key        """
+                      -----BEGIN RSA PRIVATE KEY-----
+                      MIIEogIBAAKCAQEAiCibBsb6xu31bfU57wJb4tOw6mzon+bY183pkt2CKGYA8l1n
+                      nc7hZK0KXg/1PzA7UwnN3IOuQ46jVZOE/m0kGhQs3E0OLxz2fWuRAMwvjMms6ibM
+                      xzHO5xTYwTHAqUioyY1/NtRfOv6QnXIWWUloY69KYHC/0yOX4F3sS2u5MtD61QFm
+                      DFTgj7KEfMLX4z1n/z+pMyKDZXmGfsMLBlzAC02QZddOf7OlqXwjf+JpqKkdQf/J
+                      V2zWqbVmbXRj/XX3WNoebsgdqG0NV80Td1dYdLhegvjaDdR3tCdYmKBMEWsU/a4f
+                      tRYGbqPX4odcFyzbTvYmrXfnDldec7WN3Htl1QIDAQABAoIBAF7GdPKIurKRnI7H
+                      bWYS7Ea9N55V5K65DyNYL0eNbDYWmn4ZyjAsevOLB3ZmAT3UotawMl1WQ4y+0q6U
+                      mrRG4CRO+tL1x/O/Y0v1/d7iQg7rqrLqAwx8fRqYhjAkI4kyDFsPZQeTWB5GZ/9Q
+                      FIJd+I26zDjJAp1DX0pL1ljBSQ43IykHTF1skJeZZGkix0eO9yf8Ocsrd0axj0JJ
+                      JUCsKvChv2Xb3gAPBxcu8WbnpKRuVvl0JPXwkK4nnMKaKUGE3zAzlBlw7u4lqtlu
+                      S77vvFkjHxdbd/iSvuQmQg9Ll6sPg4TbrnkcCndzzXu++mmrI2gJD4W23zoU4BOS
+                      S9Kwi9kCgYEA5xVGUWpOl+Vhd7YAUJhitk95Pvt+XMiC7qmJSzot5arA/YfZjxIg
+                      oGFecKBkuWsPB8BAE4HEqRTVkozprngWQ08ZHkfZxiUTwDwsC2IlkSl2UbNM1DUn
+                      +YVRCS++ycHKTmshzuodBeS3cQxDpJR10OhGCAvyeJGxlRxsHYQS4JcCgYEAltcS
+                      yRgAkigiOY5Gnf71ad9YJNYRKd36I9lDie+1hxT/umNGqyW2/GcSFDdsHWD+6Mgj
+                      I2B5PMB5/YyeFPUX3jXkZfS7rR72FCFs24SdSzNVCgZq8NMDWCy4fOEQye/IYXID
+                      6EBeejeS92wkdUa6J57zD1cJ911033uSIGTRznMCgYEAmgPz+g+AknyvmboUO6NV
+                      J22QwgmdDvoVSjx05U7BiHFmb5Q7zL+oPzymVSqR94MDYYchLd8v1AGu1x5UIZSo
+                      QfRWKXh6DTZpE5cHRA8GOtoHoix+6HEFU6kneZf48T/YNqvwvJgNAACQwygJbYgF
+                      fldRVcugr/trAJcQ+Bsu+cECf2Qqbk8boUVtYUzXLg29QTsNFXtgrAUhYvprTG5M
+                      wD7zst4TDtqYMOtrhpXmN+VKg/wQ60SSy++L4XZ96nwARdlJ8GHEItzTspWrnJ4p
+                      ckp9y/rcSCej+JSVHe3Ph1aR5H7RN8cC97oxLWcgaRV34iZtZvrI0dVyOiot5Nue
+                      F7UCgYEAgCyKtFPFXTGgGpUVVdD7qvbt5lh3+eAN2EgUIFu6bn+X+t4p1EXXK4ED
+                      E25nYe8lbUQC7yZaKHoa83MupJ1ideBR4asIu9jZp88UJlG3SKftucSx07xZVYqG
+                      ZUrSo7mF2SQO0bv13KTB1BCWb2SoQr6Jy6beVMFFFQii5LaMB6M=
+                      -----END RSA PRIVATE KEY-----
+                      """
+
+  @signature_without_query "DE1GkSlp4UtQkIBBATpBnzyyr7hGLzwV39CrN~VHRgWCA9pnlrW5KW8qkARdK7ZvuQ21L5dLJ39hYzfFguY-3jVVXEnhD9Vi~ilVXypM84E7K3HZvOev0ZFJexSDXLFUGqrh8WcU~CTvX-RNywY3SLLQEIXjufPQbhNqIvRs-jFnQfEw1dn2KMYpFQ1xMFYUygMxT0yccE0OHnjBA94LOmpyDs42ZS3LVmeD~dPlnCMfJL14mV9awgNqN5rlfdhG2I~STDNA4qNadG~-BpWiTq6L3DnM2ZmqdDq-fE4QbBY7MyUJILnUyRVzojpaHJ1T-6ibXRvwdFAxJucefCkbZA__"
+  @signature_with_query    "AFkIdETPTReVQFNRO4jJkEn942YktXEtQmEpAc8r02~ttCXpHuMomuzZzHanDkG-NBpqc86iohNQ25vICobDqtl3-VWFn~ZjESBFl2oUxLERryCAWh--Ffi7Q7p2~~dFgQ64~Vt9H8JmEuHx9otBFVroF97FfK9JbyGyId965T1RwC0FG-0OMHM1YsJSO9hY0iI5iTbzY-HyPIcLiYzYUPaD2~RVGPLQclnKP0oHprWfsde5zdfl5hIRG6HH3qmWPN5tr61PNQ~vdNqlfvs14B1oLZvcSqz17BZCLzpyKxwz8jv4qGpGH9JfsA599js6pUj2BI2xkcfle1XXAj2K-Q__"
+
+  defp assert_queries(query_list, expected_signature) do
+    [
+      {"Expires"    , expires    },
+      {"Signature"  , signature  },
+      {"Key-Pair-Id", key_pair_id},
+    ] = query_list
+    assert expires     == Integer.to_string(@expires_in_seconds)
+    assert signature   == expected_signature
+    assert key_pair_id == @key_pair_id
+  end
+
+  defp assert_base_url_and_get_query_params(url) do
+    %URI{host: host, path: path, query: query, scheme: scheme} = URI.parse(url)
+    assert "#{scheme}://#{host}#{path}" == @resource_url
+    URI.query_decoder(query) |> Enum.to_list()
+  end
+
+  setup do
+    lifetime = 60
+    :meck.expect(System, :system_time, fn :second -> @expires_in_seconds - lifetime end)
+    %{lifetime: lifetime}
+  end
+
+  test "should generate a signed URL for URL without query", %{lifetime: lifetime} do
+    signed_url = CloudfrontSignedUrl.get_signed_url(@resource_url, lifetime, @key_pair_id, @private_key)
+    query_params = assert_base_url_and_get_query_params(signed_url)
+    assert_queries(query_params, @signature_without_query)
+  end
+
+  test "should generate a signed URL for URL with query", %{lifetime: lifetime} do
+    signed_url = CloudfrontSignedUrl.get_signed_url(@resource_url <> "?hello=world", lifetime, @key_pair_id, @private_key)
+    query_params = assert_base_url_and_get_query_params(signed_url)
+    [{"hello", "world"} | tail] = query_params
+    assert_queries(tail, @signature_with_query)
+  end
+end
