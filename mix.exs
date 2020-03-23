d22f9894c0cb03c3f69c4d4c9a5713673aa6c99a
Takayuki Kobayashi <takayuki.kobayashi@access-company.com>
2020-03-23 11:30:56 +0900

    [176138] upgrade hackney (#116)

--- mix.exs
+++ mix.exs
@@ -70,7 +70,7 @@ defmodule Antikythera.Mixfile do
       # these are considered as implementation details of antikythera and thus must not be used by gear implementations.
       {:cowboy      , "2.6.3" , [antikythera_internal: true]},
       {:cowlib      , "2.7.3" , [antikythera_internal: true]},
-      {:hackney     , "1.14.3", [antikythera_internal: true]},
+      {:hackney     , "1.15.2", [antikythera_internal: true]},
       {:calliope    , "0.4.1" , [antikythera_internal: true]}, # 0.4.2 is broken!
       {:pool_sup    , "0.6.1" , [antikythera_internal: true]},
       {:raft_fleet  , "0.10.1", [antikythera_internal: true]},
@@ -102,11 +102,11 @@ defmodule Antikythera.Mixfile do
 
       # indirect deps
       {:ranch              , "1.7.1" , [indirect: true]}, # cowboy
-      {:certifi            , "2.4.2" , [indirect: true]}, # hackney
-      {:ssl_verify_fun     , "1.1.4" , [indirect: true]}, # hackney
+      {:certifi            , "2.5.1" , [indirect: true]}, # hackney
+      {:ssl_verify_fun     , "1.1.5" , [indirect: true]}, # hackney
       {:idna               , "6.0.0" , [indirect: true]}, # hackney
       {:metrics            , "1.0.1" , [indirect: true]}, # hackney
-      {:mimerl             , "1.0.2" , [indirect: true]}, # hackney
+      {:mimerl             , "1.2.0" , [indirect: true]}, # hackney
       {:parse_trans        , "3.3.0" , [indirect: true]}, # certifi
       {:unicode_util_compat, "0.4.1" , [indirect: true]}, # idna
       {:p1_utils           , "1.0.13", [indirect: true]}, # fast_xml
