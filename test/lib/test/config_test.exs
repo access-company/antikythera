6605e60b5bf1b6cd4efd0a15b049289d485500a2
Yuki Sekiguchi <yuki.sekiguchi@access-company.com>
2020-04-01 17:44:39 +0900

    [201739] Fix review comments

--- test/lib/test/config_test.exs
+++ test/lib/test/config_test.exs
@@ -1,17 +1,16 @@
 # Copyright(c) 2015-2020 ACCESS CO., LTD. All rights reserved.
 
-Antikythera.Test.Config.init()
-
 defmodule Antikythera.Test.ConfigTest do
   use Croma.TestCase
 
   test "blackbox_test_secret/0 should read BLACKBOX_TEST_SECRET_JSON environment variable" do
     env_var_name = "BLACKBOX_TEST_SECRET_JSON"
     original = System.get_env(env_var_name)
-    System.put_env(env_var_name, "{\"foo\": \"bar\"}")
-    assert Antikythera.Test.Config.blackbox_test_secret()["foo"] == "bar"
-    if original != nil do
-      System.put_env(env_var_name, original)
+    System.put_env(env_var_name, ~S({"foo": "bar"}))
+    assert Config.blackbox_test_secret()["foo"] == "bar"
+    case original do
+      nil -> System.delete_env(env_var_name)
+      v   -> System.put_env(env_var_name, v)
     end
   end
 end
