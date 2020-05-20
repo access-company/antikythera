30b24d726f96b681990a4ea6f2956e69951e0ad1
Yuki Sekiguchi <yuki.sekiguchi@access-company.com>
2020-05-20 17:43:10 +0900

    [202670] Use Antikythera.NodeId in Cluster.NodeId to make sure that it matches NodeId pattern

--- core/cluster/node_id.ex
+++ core/cluster/node_id.ex
@@ -10,18 +10,20 @@ defmodule AntikytheraCore.Cluster.NodeId do
   in order to make it short by truncating common parts.
   """
 
-  @type t :: String.t
+  alias Antikythera.NodeId
+
   @table_name AntikytheraCore.Ets.SystemCache.table_name()
   @key        :node_id
 
   defun init() :: :ok do
     {:ok, h} = :inet.gethostname()
     node_id = List.to_string(h)
+    true = NodeId.valid?(node_id)
     :ets.insert(@table_name, {@key, node_id})
     :ok
   end
 
-  defun get() :: t do
+  defun get() :: NodeId.t do
     :ets.lookup_element(@table_name, @key, 2)
   end
 end
