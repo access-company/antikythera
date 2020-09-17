0ac9525241f6bd5f121933d66b6acee42cfba2fc
aMasaki.Takahashi <Masaki.Takahashi11@access-company.com>
2020-09-17 11:15:10 +0900

    Add locals_without_parens option for defun and defunp

--- .formatter.exs
+++ .formatter.exs
@@ -1,3 +1,4 @@
 [
-  inputs: ["{mix,.formatter}.exs", "{core,eal,lib,local,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{core,eal,lib,local,test}/**/*.{ex,exs}"],
+  locals_without_parens: [defun: 2, defunp: 2]
 ]
