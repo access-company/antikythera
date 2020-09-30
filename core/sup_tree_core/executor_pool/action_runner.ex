af48f8593053f42ae500a2cde22971bf8f3a64c7
Fumitoshi Morisato <fumitoshi.morisato@access-company.com>
2020-09-30 16:45:38 +0900

    Fix for brutal exit reason changed from :killed to :kill

--- core/sup_tree_core/executor_pool/action_runner.ex
+++ core/sup_tree_core/executor_pool/action_runner.ex
@@ -66,7 +66,7 @@ defmodule AntikytheraCore.ExecutorPool.ActionRunner do
         Process.unlink(pid)
         Process.exit(pid, :kill)
         GearError.error(conn, :timeout, [])
-      :exit, {:killed, {GenServer, :call, [^pid | _]}} ->
+      :exit, {:kill, {GenServer, :call, [^pid | _]}} ->
         # `pid` has been brutally killed by someone, probably due to heap limit violation.
         # We additionally consume the EXIT message in mailbox, just to be sure that it won't result in memory leak.
         receive do
