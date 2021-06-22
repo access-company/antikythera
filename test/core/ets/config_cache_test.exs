d6e74f5c3f6d9e49c282966cce760f0a45debbc6
irisTa56 <takayuki.kobayashi@access-company.com>
2021-06-22 15:00:41 +0900

    [228684] test raising error

---
+++ test/core/ets/config_cache_test.exs
@@ -0,0 +1,15 @@
+# Copyright(c) 2015-2021 ACCESS CO., LTD. All rights reserved.
+
+defmodule AntikytheraCore.Ets.ConfigCacheTest do
+  use Croma.TestCase
+  alias ConfigCache.Gear
+
+  describe "ConfigCache.Gear.read/1" do
+    # normal behaviors are tested in AntikytheraCore.Config.GearTest
+    test "should raise CaseClauseError if :antikythera is given as gear name" do
+      assert_raise CaseClauseError, fn ->
+        Gear.read(:antikythera)
+      end
+    end
+  end
+end
