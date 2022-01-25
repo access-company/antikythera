a102b80509f60da08468074f1eca89ac73ebd307
Fumitoshi Morisato <fumitoshi.morisato@access-company.com>
2022-01-25 20:43:35 +0900

    Update copyright year

--- config/config.exs
+++ config/config.exs
@@ -1,4 +1,4 @@
-# Copyright(c) 2015-2021 ACCESS CO., LTD. All rights reserved.
+# Copyright(c) 2015-2022 ACCESS CO., LTD. All rights reserved.
 
 use Mix.Config
 
--- local/mix/upgrade_compatibility_test.ex
+++ local/mix/upgrade_compatibility_test.ex
@@ -1,4 +1,4 @@
-# Copyright(c) 2015-2021 ACCESS CO., LTD. All rights reserved.
+# Copyright(c) 2015-2022 ACCESS CO., LTD. All rights reserved.
 
 defmodule Mix.Tasks.AntikytheraLocal.UpgradeCompatibilityTest do
   @shortdoc "Runs tests to ensure that antikythera's upgrade doesn't break backward-compatibility"
