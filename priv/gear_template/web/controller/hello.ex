f9622e680eba4273af4106941869f6cd2680590d
Yuya Shiratori <yuya.shiratori@access-company.com>
2024-07-08 17:04:18 +0900

    [289509] Add parameter validation to gear template

--- priv/gear_template/web/controller/hello.ex
+++ priv/gear_template/web/controller/hello.ex
@@ -1,8 +1,23 @@
+use Croma
+
 defmodule <%= gear_name_camel %>.Controller.Hello do
   use Antikythera.Controller
 
-  def hello(conn) do
-    <%= gear_name_camel %>.Gettext.put_locale(conn.request.query_params["locale"] || "en")
+  defmodule QueryParams do
+    defmodule LocaleString do
+      use Croma.SubtypeOfString, pattern: ~r/\A[A-Za-z0-9-]{1,10}\z/, default: "en"
+    end
+
+    use Antikythera.ParamStringStruct,
+      fields: [
+        locale: LocaleString
+      ]
+  end
+
+  plug Antikythera.Plug.ParamsValidator, :validate, query_params: QueryParams
+
+  defun hello(%Conn{assigns: %{validated: validated}} = conn) :: v[Conn.t()] do
+    <%= gear_name_camel %>.Gettext.put_locale(validated.query_params.locale)
     Conn.render(conn, 200, "hello", [gear_name: :<%= gear_name %>])
   end
 end
