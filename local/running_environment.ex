715759e7f891761f81165ca0716b81bfe37c2529
Yuki Sekiguchi <yuki.sekiguchi@access-company.com>
2025-02-28 19:06:24 +0900

    [265200] More debug

--- local/running_environment.ex
+++ local/running_environment.ex
@@ -151,12 +151,12 @@ defmodule AntikytheraLocal.RunningEnvironment do
     rename_dir!(build_gear_dir, Path.join(@compiled_gears_dir, "#{gear_name_str}-#{version}"))
 
     :timer.sleep(5000)
-    System.cmd("find", ["."], cd: @compiled_gears_dir) |> IO.puts()
+    System.cmd("find", ["."], cd: @compiled_gears_dir) |> inspect() |> IO.puts()
       System.cmd(
         "tar",
         ["-czhf", "#{gear_name_str}-#{version}.tgz", "#{gear_name_str}-#{version}", "--warning=no-file-removed"],
         cd: @compiled_gears_dir
-      )
+      ) |> inspect() |> IO.puts()
 
     {gear_name_str, version}
   end
